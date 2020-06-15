50e27edae568d920702cedcba9ee359aa78a235e
Qiaoshan Cai <qiaoshan.cai@mystream.fr>
2020-06-15 14:41:41 +0200

    qiaoshan add java grotest

---
+++ test001_old/src/test/java/org/geotools/test001/AppTest.java
@@ -0,0 +1,20 @@
+package org.geotools.test001;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
